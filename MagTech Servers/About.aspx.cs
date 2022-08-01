bf1e2f9bf219d03b6141b97caa76f45207ac0d89
Administrator <Administrator@EC2AMAZ-VCR7K82>
2022-08-01 01:08:58 +0000

    pushing code to github

---
+++ MagTech Servers/About.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MagTech_Servers
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
